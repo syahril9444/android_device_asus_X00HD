ac39be6467237b17448439b7ffb740ba32a31259
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-09 15:27:58 +0100

    X00HD: fix device name typos from dummy dt generator

--- lineage_X00HD.mk
+++ lineage_X00HD.mk
@@ -22,19 +22,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from X00HD_4 device
+# Inherit from X00HD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00HD
-PRODUCT_MANUFACTURER := asus
+PRODUCT_MANUFACTURER := Asus
 PRODUCT_NAME := lineage_X00HD
-PRODUCT_MODEL := X00HD
+PRODUCT_MODEL := Zenfone 4 Max
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00HD
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="msm8937_64-user 9 PKQ1.181203.001 16.0200.1907.18-20190817 release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="msm8937_64-user 9 PKQ1.181203.001 16.0200.1907.18-20190817 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := asus/WW_Phone/ASUS_X00HD_4:9/PKQ1.181203.001/16.0200.1907.18-20190817:user/release-keys
