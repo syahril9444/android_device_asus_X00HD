b48bd81d2a10f460777e4ee446c6f806807571ec
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-09 14:42:25 +0100

    X00HD: build graphics HALs

--- device.mk
+++ device.mk
@@ -115,6 +115,26 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.1-service
 
+# Display
+PRODUCT_PACKAGES += \
+    AdvancedDisplay \
+    copybit.msm8937 \
+    hwcomposer.msm8937 \
+    memtrack.msm8937 \
+    liboverlay \
+    libdisplayconfig \
+    libqdMetaData.system \
+    libtinyxml
+	
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+	
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
