03d5ac24419888b2ee9a83c8386037af2e2ea0af
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-09 15:18:23 +0100

    X00HD: build ipacm

--- device.mk
+++ device.mk
@@ -200,6 +200,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ims-ext-common
 
+# IPA Manager
+PRODUCT_PACKAGES += \
+    ipacm \
+    ipacm-diag \
+    IPACM_cfg.xml
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
